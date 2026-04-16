8dadfd210c449abbadc3559d74a8bd48e31fcd66
KrArsen <arsenkr2077@gmail.com>
2026-04-17 00:15:07 +0300

    lab3

--- lab3/lab3/obj/Debug/net10.0/lab3.AssemblyInfo.cs
+++ lab3/lab3/obj/Debug/net10.0/lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5ccb5bb0a600171ee41702187abf681fa5c2aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a38e2ccedce919919a828c12f4543ed6d6da9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
